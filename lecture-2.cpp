0b32f0fcdd10744ff02ea0a92f745f3d8ebc0d3e
Aditya Tiwari <113223461+adityatiwari707@users.noreply.github.com>
2024-07-11 01:41:33 +0530

    Create lecture-2.cpp

---
+++ lecture-2.cpp
@@ -0,0 +1,12 @@
+// In this lecture, I learn what is syntax, comments in cpp, types of streams and write first cpp code.
+
+#include<iostream>    // This line means header
+
+using namespace std;  // std make sure thst cout is a part of "iostream"
+
+int main()     // we have written a main "function"
+{
+    cout << "Hello World!";
+
+    return 0;
+}
