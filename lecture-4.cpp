b0501e78ddd59ea770e3fd23c44bba0cf7d41327
Aditya Tiwari <113223461+adityatiwari707@users.noreply.github.com>
2024-07-15 01:24:41 +0530

    Create lecture-4.cpp

---
+++ lecture-4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //Declare 2 variable and then assign them values of 3,5 and then add them
+    int a = 3;
+    int b = 5;
+    //int c;
+    //c=a+b;
+
+    //cout<<c;  // if we do not want to store the value then we can write a+b in place of c.
+    cout<<a+b<<endl<<"a+b";   //endl is use to print from next line
+    //here we us a+b 2 times because we have 2 print 8 in first line and a+b in 2nd line
+    return 0;
+}
